3f06c19fd927ac19aff3c2703dae9a9a57387672
moksi3 <simon.mok@oocl.com>
2021-12-01 21:17:14 +0800

    [oop][simon mok] feat: add practice 9

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,30 @@
 package practice09;
 
-public class Student {
+
+public class Student extends Person {
+    Klass klass;
+
+    public Student(int id , String name , int age , Klass klass)
+    {
+        super(id , name , age);
+        this.klass = klass;
+    }
+
+    public String introduce()
+    {
+        String result = super.introduce();
+        result += String.format(" I am a Student.");
+
+
+        if(this.equals(this.klass.getLeader()))
+            result += String.format(" I am Leader of %s." , klass.getDisplayName());
+        else
+            result += String.format(" I am at %s." , klass.getDisplayName());
+        return result;
+    }
+
+    public Klass getKlass()
+    {
+        return this.klass;
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,77 @@
 package practice09;
 
-public class Teacher {
+
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+
+    Klass klass;
+    LinkedList<Klass> classes;
+
+    public Teacher(int id , String name , int age , LinkedList<Klass> classes)
+    {
+        super(id , name , age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id , String name , int age)
+    {
+        super(id , name , age);
+        this.klass = null;
+    }
+
+
+    public LinkedList<Klass> getClasses()
+    {
+        return this.classes;
+    }
+    public String introduce()
+    {
+        String result = super.introduce();
+        result += " I am a Teacher.";
+        boolean first=true;
+
+        if(classes==null || classes.size() == 0)
+        {
+            result += " I teach No Class.";
+        }
+        else
+        {
+            result += " I teach Class ";
+            for(Klass k : classes)
+            {
+                if(!first)
+                    result += ", ";
+                result += k.getNumber();
+                first=false;
+            }
+            result += ".";
+        }
+
+        return result;
+    }
+
+
+    public String introduceWith(Student student)
+    {
+        String result = super.introduce();
+        result += " I am a Teacher.";
+
+        if(this.isTeaching(student))
+            result += " I teach ";
+        else
+            result += " I don't teach ";
+        result += student.getName() + '.';
+
+        return result;
+
+    }
+
+    public boolean isTeaching(Student student)
+    {
+        for(Klass k : classes)
+            if(student.getKlass().getNumber() == k.getNumber())
+                return true;
+        return false;
+    }
 }

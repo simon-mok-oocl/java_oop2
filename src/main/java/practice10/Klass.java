f9f14bb5787b39dc2b63a0d412cef99bbd60467c
moksi3 <simon.mok@oocl.com>
2021-12-01 21:44:40 +0800

    [oop][simon mok] feat: add practice 10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,60 @@
 package practice10;
 
+
+
+import java.util.ArrayList;
+
 public class Klass {
+    int klass;
+    Teacher teacher;
+    Person leader;
+    ArrayList<Student> members= new ArrayList();
+
+    public Klass(int klass)
+    {
+        this.klass = klass;
+    }
+
+    public int getNumber()
+    {
+        return this.klass;
+    }
+
+    public String getDisplayName()
+    {
+        return String.format("Class %d" , this.klass);
+    }
+
+    public void assignLeader(Person leader)
+    {
+        if (members.contains(leader)) {
+            this.leader = leader;
+            if(teacher != null)
+                System.out.printf("I am %s. I know %s become Leader of %s.\n" , teacher.getName() , leader.getName() , this.getDisplayName());
+        } else {
+            System.out.printf("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student member) {
+        System.out.println("1111111------------------------------------------");
+        System.out.println(!members.contains(member));
+
+        if (!members.contains(member)) {
+            members.add(member);
+            if(teacher != null )
+                System.out.printf("I am %s. I know %s has joined %s.\n", teacher.getName(), member.getName(), this.getDisplayName());
+
+        }
+    }
+
+    public Person getLeader()
+    {
+        return this.leader;
+    }
+
+    public void setTeacher(Teacher teacher)
+    {
+        this.teacher = teacher;
+    }
 }
--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,39 @@
 package practice10;
 
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id , String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce()
+    {
+        return String.format("My name is %s. I am %d years old." , this.name , this.age);
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        else
+            return this.id == ((Person)obj).getId();
+    }
+
 }

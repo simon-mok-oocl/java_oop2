b1cbd309c31d52ad01ac48543095f31c417f9791
moksi3 <simon.mok@oocl.com>
2021-12-01 20:23:45 +0800

    [oop][simon mok] feat: add practice 7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,32 @@
 package practice07;
 
 public class Klass {
+    int klass;
+    Person leader;
+
+    public Klass(int klass)
+    {
+        this.klass = klass;
+    }
+
+    public int getNumber()
+    {
+        return this.klass;
+    }
+
+    public String getDisplayName()
+    {
+        return String.format("Class %d" , this.klass);
+    }
+
+    public void assignLeader(Person leader)
+    {
+        this.leader = leader;
+    }
+
+    public Person getLeader()
+    {
+        return this.leader;
+    }
 }
+

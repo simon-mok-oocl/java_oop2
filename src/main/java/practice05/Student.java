6f11dbbd1342b88ce4f01e79cf0610c211df09e5
moksi3 <simon.mok@oocl.com>
2021-12-01 18:02:56 +0800

    [oop][simon] feat: add practice 5

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,24 @@
 package practice05;
 
-public class Student {
+
+public class Student extends Person {
+    int klass;
+
+    public Student(String name , int age , int klass)
+    {
+        super(name , age);
+        this.klass = klass;
+    }
+
+    public String introduce()
+    {
+        String result = super.introduce();
+        result += String.format(" I am a Student. I am at Class %d." , this.klass);
+        return result;
+    }
+
+    public int getKlass()
+    {
+        return this.klass;
+    }
 }
--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,32 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    int klass;
+    public Teacher(String name, int age , int klass)
+    {
+        super(name , age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age)
+    {
+        super(name , age);
+        this.klass = 0;
+    }
+
+    public int getKlass()
+    {
+        return this.klass;
+    }
+
+    public String introduce()
+    {
+        String result = super.introduce();
+
+        if(this.klass <= 0)
+            result += " I am a Teacher. I teach No Class.";
+        else
+            result += String.format(" I am a Teacher. I teach Class %d." , this.klass);
+
+        return result;
+    }
 }

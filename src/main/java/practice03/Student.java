9cf9afc64073a2998e2493f3d99a34e025fd2245
moksi3 <simon.mok@oocl.com>
2021-12-01 17:28:57 +0800

    [oop2][simon mok] feat: add practice 3

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,23 @@
 package practice03;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+    int klass;
+
+    public Student(String name , int age , int klass)
+    {
+        super(name , age);
+        this.klass = klass;
+    }
+
+    public String introduce()
+    {
+        return String.format("I am a Student. I am at Class %d." , this.klass);
+    }
+
+    public int getKlass()
+    {
+        return this.klass;
+    }
 }
